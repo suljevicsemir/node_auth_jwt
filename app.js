2044a04e72cbc83f9ebedd3e1880c85c73290df3
suljevicsemir <suljevic.semir@gmail.com>
2021-03-21 14:58:14 +0100

    Cookies parser

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const app = express();
+const cookieParser = require('cookie-parser');
+
 
 app.use(express.json());
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
-
+app.use(cookieParser());
 
 
 const dbURL = "mongodb+srv://semir:pa6Brnzq4VrgGxg@cluster0.xrc1d.mongodb.net/net_ninja?retryWrites=true&w=majority";
@@ -42,3 +44,23 @@ app.get('/smoothies', (req, res) => {
 })
 
 app.use(authRoutes);
+
+
+app.get('/set-cookies', (req, res) => {
+
+    //res.setHeader('Set-Cookie', 'newUser=true');
+
+    res.cookie('newUser', false);
+    res.cookie('isEmployee', true, {
+        maxAge: 1000 * 60 * 60 * 24
+    });
+
+    res.send('you got the cookies');
+
+});
+
+app.get('/read-cookies', (req, res) => {
+    const cookies = req.cookies;
+    console.log(cookies.isEmployee);
+    res.json(cookies);
+});
