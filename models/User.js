ac01491580c7d93583308f108c207e5e04541ea3
suljevicsemir <suljevic.semir@gmail.com>
2021-03-21 14:41:11 +0100

    Password hashing and auth views

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const {isEmail} = require('validator');
-
+const bcrypt = require('bcrypt');
 const userSchema = new mongoose.Schema({
     email : {
         type: String,
@@ -15,6 +15,16 @@ const userSchema = new mongoose.Schema({
         minlength: [6, 'Minimum password length is 6 characters']
     }
 });
+
+
+
+userSchema.pre('save', async function(next) {
+
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
 
 const User = mongoose.model('user', userSchema);
 
